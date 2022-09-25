de2d1e655a5bb91700d9ebe9bb572d4814597fd4
xm-tech <maxm09@163.com>
2022-09-25 11:38:50 +0800

    init map

--- main.c
+++ main.c
@@ -3,13 +3,32 @@
 
 #define CHESS_NUM 14;
 
-// 棋子
+// 格子直线
+#define dot "."
+#define line "----------------------------------------------------------------"
+#define column "|"
+#define line_seperator "\n"
+// 格子宽度和长度
+#define grid_size 30
+#define grid_x_num 10
+#define grid_y_num 12
+
+const int red_chess[] = {};
+const int black_chess[] = {};
+
+// 棋子坐标
 typedef struct {
 	int x;
 	int y;
+} position;
+
+// 棋子
+typedef struct {
 	int chessid;
 	// 阵营: 0-楚，1-汉
 	int camp;
+	// 棋子位置
+	position pos;
 } chess;
 
 // 棋盘
@@ -18,6 +37,7 @@ typedef struct {
 } map;
 
 
+// 初始化棋盘
 void initMap();
 
 // 初始化棋子
@@ -34,8 +54,29 @@ int main(){
 
 void initMap() {
 	printf("初始化棋盘 \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+	printf("\t |    |    |    |    |    |    |    |    | \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+	printf("\t |    |    |    |    |    |    |    |    | \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+	printf("\t |    |    |    |    |    |    |    |    | \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+	printf("\t |    |    |    |    |    |    |    |    | \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+	printf("\t |     楚河                     汉界     | \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+	printf("\t |    |    |    |    |    |    |    |    | \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+	printf("\t |    |    |    |    |    |    |    |    | \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+	printf("\t |    |    |    |    |    |    |    |    | \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+	printf("\t |    |    |    |    |    |    |    |    | \n");
+	printf("\t |----|----|----|----|----|----|----|----| \n");
+
 }
 
+	
 void initChess() {
 	printf("初始化棋子 \n");
 }
